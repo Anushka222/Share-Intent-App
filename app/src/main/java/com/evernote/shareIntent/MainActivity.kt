fadc296fc8d60f3a25cc52d98cd31f6ad4a80e07
anushka <asharma3@evernote.com>
2022-05-25 11:28:22 +0530

    final intent app code

--- app/src/main/java/com/evernote/shareIntent/MainActivity.kt
+++ app/src/main/java/com/evernote/shareIntent/MainActivity.kt
@@ -4,10 +4,11 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,33 +16,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        displayData()
+        displayData(intent)
     }
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
     override fun onNewIntent(newIntent : Intent?) {
         super.onNewIntent(newIntent)
-        displayData()
+        if (newIntent != null) {
+            displayData(newIntent)
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP_MR1)
-    fun displayData() {
+    fun displayData(intent: Intent) {
+        val shareExtension = ShareExtensionUtil()
         val  textView1= findViewById<TextView>(R.id.textView1)
         val  textView2 = findViewById<TextView>(R.id.textView2)
         val  textView3= findViewById<TextView>(R.id.textView3)
         val  textView4 = findViewById<TextView>(R.id.textView4)
         val  textView5 = findViewById<TextView>(R.id.textView5)
-        val  textView6 = findViewById<TextView>(R.id.textView6)
-        val shareExtension = ShareExtensionUtil()
+        val  imageView = findViewById<ImageView>(R.id.imageView)
         var arguments = shareExtension.createShareIntentData(this, intent)
-        if(arguments!=null){
-            textView1.text = "intentDataType = " + arguments.getString("intentDataType")
-            textView2.text = "intentDataContent = "+ arguments.getString("intentDataContent")
-            textView3.text = "extraData = "+ arguments.getString("extraData")
-            textView4.text = "title = " + arguments.getString("title")
-            textView5.text = "intentDataReferer = " + arguments.getString("intentDataReferer")
-            textView6.text = "intentDataIcon = " + arguments.getString("intentDataIcon")
-        } else {
-            Log.d("ShareExtension:", "Init intent type is null")
+        if (arguments != null) {
+            if(arguments.getString("intentDataIcon")!=null){
+           var iconUrl= arguments.getString("intentDataIcon")!!
+                Glide.with(this)
+                    .load(iconUrl)
+                    .into(imageView)
+            }
+            else{
+                Glide.with(this)
+                    .load(R.drawable.data_icon)
+                    .into(imageView)
+            }
+
+            textView1.text = arguments.getString("intentDataType")
+            textView2.text = arguments.getString("intentDataContent")
+            textView3.text = arguments.getString("extraData")
+            textView4.text = arguments.getString("title")
+            textView5.text = arguments.getString("intentDataReferer")
+        }
+        else {
+                Log.d("ShareExtension:", "Init intent type is null")
         }
     }
 }
